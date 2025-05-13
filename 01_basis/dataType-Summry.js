0a471b1dd0a97ae85814ed780fd4084a98bcc006
Pawan Gupta <erpawan459@gmail.com>
2025-05-13 16:50:04 +0000

    Basis code

---
+++ 01_basis/dataType-Summry.js
@@ -0,0 +1,39 @@
+// data type basically two type ......
+// 1- Primitive data type, 
+
+// 7 type (String, Number, Boolean, BigInt, Symbol, Null , Undefine )
+
+let score = 100
+let age = 25
+let isLogin = true
+let bigNumber = 123456789987465123n
+let balance;
+let account ="null"
+const userId = Symbol("123")
+const anotherId =Symbol("123")
+
+// console.log(userId==anotherId);
+// console.log(typeof bigNumber);
+// console.log(bigNumber);
+
+
+
+// 2- Reference (Non Primitive ) data type
+// 3 type (Array, Object, Function)
+
+const heros = ["Sunny","Akshay","Sunil","Mithun"];
+
+//console.log(heros[2]);
+
+let myObj = {
+    name: "Pawan",
+    age: 25
+}
+//console.log(myObj);
+
+const myFunction =function(){
+    console.log("Hello World");
+    
+}
+log
+
