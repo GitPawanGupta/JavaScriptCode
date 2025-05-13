3f61fbafe616ca294d1f37e9a61bc11f7fade713
Pawan Gupta <erpawan459@gmail.com>
2025-05-13 06:51:43 +0000

    basis CODE

---
+++ 01_basis/01_variable.js
@@ -0,0 +1,24 @@
+const accountId =123456
+let accountEmail="pawan@gmail.com"
+var accountPassword="456123"
+let accountState;
+
+accountCity="Delhi"
+
+//accountId=3 // not allow
+
+accountEmail="gupta@gmail.com"
+accountPassword="321654"
+accountCity="Gurugram"
+let rajuId="null"
+let ramuId=null
+let pankajId=0
+let rajanId="0"
+/*
+refer not to use var
+because of issue in block scop and function scop.
+always prefer let variable
+*/
+
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail,accountCity,accountState,rajuId,ramuId,pankajId,rajanId]);
---
+++ 01_basis/02_datatype.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3)  // we are using node js , not browser
+
+console.log(3+3);  // code readbillity should be high
+console.log("pawan gupta");
+
+let name= "pawan" // string
+let age = 21 // number
+let isLogIn = true   // boolean 
+
+/*
+number => 2 to power 20
+bigint
+string => ""
+boolean => true/false
+null => stand alone value
+undefined
+symbol => unique
+
+object 
+*/
+
+// for find type of datatype 
+console.log(typeof "pawan gupta"); //string
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefine); //undefine 
+
+
+
+
