44f3c65465042b3fe49ac0fc99ef9ef147555eb2
Pawan Gupta <erpawan459@gmail.com>
2025-05-13 10:28:15 +0000

    data type conversion

---
+++ 01_basis/03_conversionOpration.js
@@ -0,0 +1,35 @@
+let score = undefined;
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let another= Number (score)
+//console.log( another);
+
+
+/*
+33 => number
+"33" => object
+"33abc" => NaN (Not an number)
+null => 0 
+undefined => NaN
+*/
+let isLogIn = undefined
+
+let booleanNumber = Boolean(isLogIn)
+//console.log( typeof booleanNumber);
+
+/*
+true  => true
+1 => true
+0=>false
+"" => false
+"Pawan" => true
+null => false
+undefined => false
+*/
+
+let number = "Pawan"
+
+let stringNumber = String (number)
+console.log( stringNumber);
