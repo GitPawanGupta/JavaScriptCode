35833323cc710c5dcfdd829e1c37a183d8a6f459
Pawan Gupta <erpawan459@gmail.com>
2025-05-13 18:52:06 +0000

    Num And Math

---
+++ 01_basis/06_nums_and_maths.js
@@ -0,0 +1,39 @@
+const score = 100
+//console.log(typeof score);
+
+//const balance = new Number(1001)
+//console.log(typeof balance);
+//console.log(score.toString().length);
+//console.log(balance.toFixed(2));
+
+const anotherNumber =2503
+//console.log(anotherNumber.toFixed(1));
+
+
+const thirdNum = 250.896
+//console.log(thirdNum.toPrecision(3));
+
+const hundred =1000000
+//console.log(hundred.toLocaleString('en-IN'));
+
+//*******************Maths**************** */
+// console.log(Math);
+// console.log(Math.abs(-8));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.max(2,3,5,8,9));
+// console.log(Math.min(2,3,4,5,9,8));
+console.log(Math.random());
+console.log(Math.random()+1);
+
+
+
+
+
+
+
+
+
+
+
